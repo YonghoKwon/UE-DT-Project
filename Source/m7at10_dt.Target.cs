6eb4b4c22408b5e80b6b0d646c59027ef2c3b1a4
Grandes <ho2708@naver.com>
2026-01-01 16:19:05 +0900

    Shipping 패키징에서 로그 파일 추가 기능 추가

--- Source/m7at10_dt.Target.cs
+++ Source/m7at10_dt.Target.cs
@@ -11,5 +11,14 @@ public class m7at10_dtTarget : TargetRules
 		DefaultBuildSettings = BuildSettingsVersion.V4;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_3;
 		ExtraModuleNames.Add("m7at10_dt");
+
+		// // [추가] 프로젝트 고유의 빌드 환경을 사용하도록 설정 (로그 설정을 덮어쓰기 위해 필요)
+		// BuildEnvironment = TargetBuildEnvironment.Unique;
+		//
+		// // Shipping 빌드에서도 로그 활성화
+		// if (Target.Configuration == UnrealTargetConfiguration.Shipping)
+		// {
+		// 	bUseLoggingInShipping = true;
+		// }
 	}
 }
--- Source/m7at10_dt/M7AT10/Core/DxGameInstance.h
+++ Source/m7at10_dt/M7AT10/Core/DxGameInstance.h
@@ -17,11 +17,20 @@ class M7AT10_DT_API UDxGameInstance : public UGameInstance
 	// Function
 public:
 	virtual void Init() override;
+	virtual void Shutdown() override;
+
+	static UDxGameInstance* GetInstance();
+
+    static void WriteCustomLog(FString LogText);
 private:
+	// 오래된 로그 파일 삭제
+	// TODO: Window 맞춤형, 재실행 되는 경우에 한하여 삭제. 계속 실행되는 경우 고려 필요
+	void DeleteOldLogs(int32 RetentionDays);
 protected:
 
 	// Variable
 public:
 private:
+	static UDxGameInstance* Instance;
 protected:
 };
