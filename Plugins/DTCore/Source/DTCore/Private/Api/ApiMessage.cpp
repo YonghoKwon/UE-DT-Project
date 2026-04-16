ce6b211444ce7be95e05230b8555094460a0534f
Grandes <ho2708@naver.com>
2026-04-16 22:12:30 +0900

    서브모듈 추가 작업

--- Plugins/DTCore/Source/DTCore/Private/Api/ApiMessage.cpp
+++
@@ -1,7 +0,0 @@
-﻿#include "Api/ApiMessage.h"
-
-
-UWorld* UApiMessage::GetWorld() const
-{
-	return GetOuter() ? GetOuter()->GetWorld() : nullptr;
-}
--- Plugins/DTCore/Source/DTCore/Public/Api/FApiDataBase.h
+++
@@ -1,7 +0,0 @@
-﻿#pragma once
-#include "CoreMinimal.h"
-
-struct FApiDataBase
-{
-	virtual ~FApiDataBase() = default;
-};
