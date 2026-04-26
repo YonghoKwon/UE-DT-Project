22070efdceaeb4c042f5ae6dbfd21c162f5b689b
YonghoKwon <ho2708@naver.com>
2026-04-26 22:27:56 +0900

    Implement sensor discovery and monitor binding manager

---
+++ Source/m7at10_dt/M7AT10/Sensor/VirtualSensorManager.cpp
@@ -0,0 +1,164 @@
+#include "Sensor/VirtualSensorManager.h"
+
+#include "Camera/VirtualCameraComp.h"
+#include "EngineUtils.h"
+#include "GameFramework/Actor.h"
+#include "Sensor/VirtualLidarSensorComp.h"
+#include "UI/VirtualSensorMonitorWidget.h"
+
+AVirtualSensorManager::AVirtualSensorManager()
+{
+    PrimaryActorTick.bCanEverTick = false;
+}
+
+void AVirtualSensorManager::BeginPlay()
+{
+    Super::BeginPlay();
+    if (bDiscoverOnBeginPlay)
+    {
+        DiscoverSensorsInLevel();
+    }
+    ApplyWidgetBinding();
+}
+
+void AVirtualSensorManager::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+    Super::EndPlay(EndPlayReason);
+}
+
+void AVirtualSensorManager::DiscoverSensorsInLevel()
+{
+    Cameras.Reset();
+    Lidars.Reset();
+    UWorld* World = GetWorld();
+    if (!World) return;
+    for (TActorIterator<AActor> It(World); It; ++It)
+    {
+        AActor* Actor = *It;
+        if (!Actor) continue;
+        TArray<UVirtualCameraComp*> FoundCameras;
+        Actor->GetComponents<UVirtualCameraComp>(FoundCameras);
+        for (UVirtualCameraComp* CameraComp : FoundCameras)
+        {
+            RegisterCamera(CameraComp);
+        }
+        TArray<UVirtualLidarSensorComp*> FoundLidars;
+        Actor->GetComponents<UVirtualLidarSensorComp>(FoundLidars);
+        for (UVirtualLidarSensorComp* LidarComp : FoundLidars)
+        {
+            RegisterLidar(LidarComp);
+        }
+    }
+}
+
+void AVirtualSensorManager::RegisterCamera(UVirtualCameraComp* CameraComp)
+{
+    if (CameraComp && !Cameras.Contains(CameraComp))
+    {
+        Cameras.Add(CameraComp);
+        ApplyWidgetBinding();
+    }
+}
+
+void AVirtualSensorManager::RegisterLidar(UVirtualLidarSensorComp* LidarComp)
+{
+    if (LidarComp && !Lidars.Contains(LidarComp))
+    {
+        Lidars.Add(LidarComp);
+        ApplyWidgetBinding();
+    }
+}
+
+void AVirtualSensorManager::BindMonitorWidget(UVirtualSensorMonitorWidget* MonitorWidget)
+{
+    BoundMonitorWidget = MonitorWidget;
+    ApplyWidgetBinding();
+}
+
+void AVirtualSensorManager::SelectCameraByIndex(int32 Index)
+{
+    if (Cameras.IsValidIndex(Index))
+    {
+        SelectedCameraIndex = Index;
+        ApplyWidgetBinding();
+    }
+}
+
+void AVirtualSensorManager::SelectLidarByIndex(int32 Index)
+{
+    if (Lidars.IsValidIndex(Index))
+    {
+        SelectedLidarIndex = Index;
+        ApplyWidgetBinding();
+    }
+}
+
+void AVirtualSensorManager::SetViewMode(EVirtualSensorViewMode NewMode)
+{
+    CurrentViewMode = NewMode;
+    if (BoundMonitorWidget)
+    {
+        if (CurrentViewMode == EVirtualSensorViewMode::Camera) BoundMonitorWidget->ShowCameraView();
+        if (CurrentViewMode == EVirtualSensorViewMode::Lidar) BoundMonitorWidget->ShowLidarView();
+    }
+    OnViewModeChanged.Broadcast(CurrentViewMode);
+}
+
+void AVirtualSensorManager::ToggleSensorView()
+{
+    SetViewMode(CurrentViewMode == EVirtualSensorViewMode::Camera ? EVirtualSensorViewMode::Lidar : EVirtualSensorViewMode::Camera);
+}
+
+void AVirtualSensorManager::StartAllSensors() {}
+void AVirtualSensorManager::StopAllSensors() {}
+void AVirtualSensorManager::CaptureAllOnce() {}
+
+UVirtualCameraComp* AVirtualSensorManager::GetSelectedCamera() const
+{
+    return Cameras.IsValidIndex(SelectedCameraIndex) ? Cameras[SelectedCameraIndex] : nullptr;
+}
+
+UVirtualLidarSensorComp* AVirtualSensorManager::GetSelectedLidar() const
+{
+    return Lidars.IsValidIndex(SelectedLidarIndex) ? Lidars[SelectedLidarIndex] : nullptr;
+}
+
+TArray<FVirtualSensorSummary> AVirtualSensorManager::GetCameraSummaries() const
+{
+    TArray<FVirtualSensorSummary> Result;
+    for (int32 Index = 0; Index < Cameras.Num(); ++Index)
+    {
+        FVirtualSensorSummary Summary;
+        Summary.Index = Index;
+        Summary.SensorType = TEXT("virtual_camera");
+        Summary.bValid = Cameras[Index] != nullptr;
+        Summary.SensorId = Cameras[Index] ? Cameras[Index]->SensorId : TEXT("");
+        Result.Add(Summary);
+    }
+    return Result;
+}
+
+TArray<FVirtualSensorSummary> AVirtualSensorManager::GetLidarSummaries() const
+{
+    TArray<FVirtualSensorSummary> Result;
+    for (int32 Index = 0; Index < Lidars.Num(); ++Index)
+    {
+        FVirtualSensorSummary Summary;
+        Summary.Index = Index;
+        Summary.SensorType = TEXT("virtual_lidar");
+        Summary.bValid = Lidars[Index] != nullptr;
+        Summary.SensorId = Lidars[Index] ? Lidars[Index]->SensorId : TEXT("");
+        Result.Add(Summary);
+    }
+    return Result;
+}
+
+void AVirtualSensorManager::ApplyWidgetBinding()
+{
+    if (!BoundMonitorWidget) return;
+    BoundMonitorWidget->BindVirtualCamera(GetSelectedCamera());
+    BoundMonitorWidget->BindVirtualLidar(GetSelectedLidar());
+}
+
+void AVirtualSensorManager::RunSynchronizedCapture() {}
+void AVirtualSensorManager::AssignSharedTransportIfPossible(UActorComponent* SensorComp) {}
