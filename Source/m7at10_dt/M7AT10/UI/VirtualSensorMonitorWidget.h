90c24ebb90cfb544353e48d74c113172dc9ec013
YonghoKwon <ho2708@naver.com>
2026-04-26 10:53:10 +0900

    Add virtual sensor monitor widget interface

---
+++ Source/m7at10_dt/M7AT10/UI/VirtualSensorMonitorWidget.h
@@ -0,0 +1,66 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "VirtualSensorMonitorWidget.generated.h"
+
+class UButton;
+class UImage;
+class UTextBlock;
+class UVirtualCameraComp;
+class UVirtualLidarSensorComp;
+
+UCLASS()
+class M7AT10_DT_API UVirtualSensorMonitorWidget : public UUserWidget
+{
+    GENERATED_BODY()
+
+public:
+    UFUNCTION(BlueprintCallable, Category = "DigitalTwin|SensorMonitor")
+    void BindVirtualCamera(UVirtualCameraComp* InCameraComp);
+
+    UFUNCTION(BlueprintCallable, Category = "DigitalTwin|SensorMonitor")
+    void BindVirtualLidar(UVirtualLidarSensorComp* InLidarComp);
+
+    UFUNCTION(BlueprintCallable, Category = "DigitalTwin|SensorMonitor")
+    void ShowCameraView();
+
+    UFUNCTION(BlueprintCallable, Category = "DigitalTwin|SensorMonitor")
+    void ShowLidarView();
+
+    UFUNCTION(BlueprintCallable, Category = "DigitalTwin|SensorMonitor")
+    void ToggleView();
+
+protected:
+    virtual void NativeConstruct() override;
+    virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
+
+private:
+    UFUNCTION()
+    void HandleToggleButtonClicked();
+
+    void BuildWidgetTreeIfNeeded();
+    void RefreshImageBrush();
+    void RefreshTitle();
+
+private:
+    UPROPERTY(Transient)
+    TObjectPtr<UVirtualCameraComp> CameraComp;
+
+    UPROPERTY(Transient)
+    TObjectPtr<UVirtualLidarSensorComp> LidarComp;
+
+    UPROPERTY(Transient)
+    TObjectPtr<UImage> ViewImage;
+
+    UPROPERTY(Transient)
+    TObjectPtr<UButton> ToggleButton;
+
+    UPROPERTY(Transient)
+    TObjectPtr<UTextBlock> TitleText;
+
+    UPROPERTY(Transient)
+    TObjectPtr<UTextBlock> ToggleButtonText;
+
+    bool bShowingLidar = false;
+};
