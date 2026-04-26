6fe01e8028653d8e17e78e0d69ad32a3f98e0702
YonghoKwon <ho2708@naver.com>
2026-04-26 22:29:09 +0900

    Add reusable sensor data transport interface

---
+++ Source/m7at10_dt/M7AT10/Sensor/VirtualSensorDataTransportComp.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "VirtualSensorDataTransportComp.generated.h"
+
+UENUM(BlueprintType)
+enum class EVirtualSensorTransportMode : uint8
+{
+    None UMETA(DisplayName = "None"),
+    LogOnly UMETA(DisplayName = "Log Only"),
+    SaveToFile UMETA(DisplayName = "Save To File"),
+    HttpPost UMETA(DisplayName = "HTTP POST")
+};
+
+USTRUCT(BlueprintType)
+struct M7AT10_DT_API FVirtualSensorTransportResult
+{
+    GENERATED_BODY()
+
+    UPROPERTY(BlueprintReadOnly, Category = "DigitalTwin|SensorTransport")
+    bool bSubmitted = false;
+
+    UPROPERTY(BlueprintReadOnly, Category = "DigitalTwin|SensorTransport")
+    FString Message;
+
+    UPROPERTY(BlueprintReadOnly, Category = "DigitalTwin|SensorTransport")
+    int32 DataLength = 0;
+
+    UPROPERTY(BlueprintReadOnly, Category = "DigitalTwin|SensorTransport")
+    FString SavedFilePath;
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnVirtualSensorDataSent, const FVirtualSensorTransportResult&, Result);
+
+UCLASS(ClassGroup = (DTCore), meta = (BlueprintSpawnableComponent))
+class M7AT10_DT_API UVirtualSensorDataTransportComp : public UActorComponent
+{
+    GENERATED_BODY()
+
+public:
+    UVirtualSensorDataTransportComp();
+
+    UFUNCTION(BlueprintCallable, Category = "DigitalTwin|SensorTransport")
+    FVirtualSensorTransportResult SendJson(const FString& SensorId, const FString& SensorType, const FString& JsonText);
+
+    UPROPERTY(BlueprintAssignable, Category = "DigitalTwin|SensorTransport")
+    FOnVirtualSensorDataSent OnDataSent;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "DigitalTwin|SensorTransport")
+    EVirtualSensorTransportMode TransportMode = EVirtualSensorTransportMode::LogOnly;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "DigitalTwin|SensorTransport", meta = (EditCondition = "TransportMode == EVirtualSensorTransportMode::HttpPost"))
+    FString HttpEndpoint;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "DigitalTwin|SensorTransport")
+    FString SaveSubDirectory = TEXT("SensorCaptures");
+
+private:
+    FVirtualSensorTransportResult SendHttp(const FString& SensorId, const FString& SensorType, const FString& JsonText) const;
+    FVirtualSensorTransportResult SaveJson(const FString& SensorId, const FString& SensorType, const FString& JsonText) const;
+};
