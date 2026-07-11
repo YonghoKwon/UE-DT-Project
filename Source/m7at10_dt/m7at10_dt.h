f02a5b856d2c968b99ee8951047a75a30d830f6d
YonghoKwon <ho2708@naver.com>
2026-07-11 09:32:25 +0900

    Remove old runtime module header file

--- Source/m7at10_dt/m7at10_dt.h
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#pragma once
-
-#include "CoreMinimal.h"
-
-// 로그 카테고리 선언
-// General: 일반 로그, Warning: 경고, Error: 에러
-DECLARE_LOG_CATEGORY_EXTERN(LogM7AT10, Log, All);
