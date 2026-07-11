e3e6a75433a325d477467a6885739c84c1aebb3c
YonghoKwon <ho2708@naver.com>
2026-07-11 09:32:17 +0900

    Remove old runtime module source file

--- Source/m7at10_dt/m7at10_dt.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "m7at10_dt.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, m7at10_dt, "m7at10_dt" );
-
-// 로그 카테고리 정의
-DEFINE_LOG_CATEGORY(LogM7AT10);
