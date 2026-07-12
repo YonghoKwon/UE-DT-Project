ff81120869b1c9c614d1d3cffd906cce9b91f4d7
YonghoKwon <ho2708@naver.com>
2026-07-13 07:49:20 +0900

    Move DxLevelManagerBasic source to MA0T10 source path

---
+++ Source/ma0t10_dt/MA0T10/Manager/DxLevelManagerBasic.cpp
@@ -0,0 +1,23 @@
+﻿
+#include "DxLevelManagerBasic.h"
+
+#include "Core/DxGameStateBase.h"
+
+
+ADxLevelManagerBasic::ADxLevelManagerBasic()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	ViewMode = EDxViewMode::Basic;
+}
+
+void ADxLevelManagerBasic::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ADxLevelManagerBasic::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
