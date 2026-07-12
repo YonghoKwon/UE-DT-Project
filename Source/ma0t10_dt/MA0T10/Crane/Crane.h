85187b364a4ea07d2d374f3cbb8426619e603ce0
YonghoKwon <ho2708@naver.com>
2026-07-13 07:41:15 +0900

    Move Crane header to MA0T10 source path

---
+++ Source/ma0t10_dt/MA0T10/Crane/Crane.h
@@ -0,0 +1,56 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ma0t10_dt/MA0T10/InteractableActor/FacilityBase.h"
+#include "Crane.generated.h"
+
+class UPoseableMeshComponent;
+class UStaticMeshComponent;
+class UCraneStatusVisualizerComp;
+class UCraneMechDriverComp;
+class UCraneDataSyncComp;
+
+UCLASS()
+class MA0T10_DT_API ACrane : public AFacilityBase
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ACrane();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Function
+public:
+private:
+protected:
+
+	// Variable
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Crane")
+	FString CraneId;
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UPoseableMeshComponent> MainMesh;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	TObjectPtr<UCraneDataSyncComp> DataSyncComp;
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	TObjectPtr<UCraneMechDriverComp> MechDriverComp;
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	TObjectPtr<UCraneStatusVisualizerComp> StatusVisualizerComp;
+
+	// UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	// TObjectPtr<UPoseableMeshComponent> CraneMesh;
+protected:
+};
