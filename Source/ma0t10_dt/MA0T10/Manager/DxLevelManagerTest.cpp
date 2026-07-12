631e6671b9485f4965516335402648269e1f5d0f
YonghoKwon <ho2708@naver.com>
2026-07-13 07:49:35 +0900

    Move DxLevelManagerTest source to MA0T10 source path

---
+++ Source/ma0t10_dt/MA0T10/Manager/DxLevelManagerTest.cpp
@@ -0,0 +1,54 @@
+﻿
+#include "DxLevelManagerTest.h"
+
+#include "DxApiServiceTest.h"
+#include "ma0t10_dt/ma0t10_dt.h"
+#include "Core/DxGameStateBase.h"
+
+
+ADxLevelManagerTest::ADxLevelManagerTest()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	ViewMode = EDxViewMode::Test;
+}
+
+void ADxLevelManagerTest::BeginPlay()
+{
+	Super::BeginPlay();
+
+	DxApiService = NewObject<UDxApiServiceTest>(this);
+	DxApiService->Initialize(GetGameInstance());
+
+	DxApiService->OnDataReady.AddDynamic(this, &ADxLevelManagerTest::OnLevelInitApiFinished);
+
+	UE_LOG(LogMA0T10, Log, TEXT("[LevelManagerTest] Start Loading..."));
+	DxApiService->LevelInitApiCall();
+}
+
+void ADxLevelManagerTest::SetupForLevel()
+{
+	Super::SetupForLevel();
+}
+
+void ADxLevelManagerTest::CleanupForLevel()
+{
+	Super::CleanupForLevel();
+}
+
+void ADxLevelManagerTest::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ADxLevelManagerTest::OnLevelInitApiFinished(bool bSuccess, const FString& Error)
+{
+	if (bSuccess)
+	{
+		UE_LOG(LogMA0T10, Log, TEXT("[LevelManagerTest] All Data Loaded! Spawning Actors"));
+	}
+	else
+	{
+		UE_LOG(LogMA0T10, Error, TEXT("[LevelManagerTest] Error: %s"), *Error);
+	}
+}
