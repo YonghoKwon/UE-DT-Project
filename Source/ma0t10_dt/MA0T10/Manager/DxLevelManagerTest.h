dc8626dee1416a9d9840e40d90b6dcf9ca8e3f3c
YonghoKwon <ho2708@naver.com>
2026-07-13 07:49:27 +0900

    Move DxLevelManagerTest header to MA0T10 source path

---
+++ Source/ma0t10_dt/MA0T10/Manager/DxLevelManagerTest.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Manager/DxLevelManagerBase.h"
+#include "DxLevelManagerTest.generated.h"
+
+class UDxApiServiceTest;
+
+UCLASS()
+class MA0T10_DT_API ADxLevelManagerTest : public ADxLevelManagerBase
+{
+	GENERATED_BODY()
+
+public:
+	ADxLevelManagerTest();
+
+	virtual void BeginPlay() override;
+
+protected:
+	virtual void SetupForLevel() override;
+	virtual void CleanupForLevel() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+	// Function
+public:
+	UFUNCTION()
+	void OnLevelInitApiFinished(bool bSuccess, const FString& Error);
+private:
+protected:
+
+	// Variable
+public:
+	UPROPERTY()
+	TObjectPtr<UDxApiServiceTest> DxApiService;
+private:
+protected:
+};
